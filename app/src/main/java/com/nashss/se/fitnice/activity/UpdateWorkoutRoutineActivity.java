fd35d64b7c3f524e59fbf639addacc6d6fc1cde8
NealAites <aitesneal@gmail.com>
2023-06-01 13:04:28 -0500

    UpdateWorkoutRoutine endpoint activity classes, decide what to do with Tags, are they a Set or a List

---
+++ app/src/main/java/com/nashss/se/fitnice/activity/UpdateWorkoutRoutineActivity.java
@@ -0,0 +1,81 @@
+package com.nashss.se.fitnice.activity;
+import com.nashss.se.fitnice.activity.requests.UpdateWorkoutRoutineRequest;
+import com.nashss.se.fitnice.activity.results.UpdateWorkoutRoutineResult;
+import com.nashss.se.fitnice.converters.VModelConverter;
+import com.nashss.se.fitnice.dynamodb.WorkoutRoutineDao;
+import com.nashss.se.fitnice.dynamodb.models.WorkoutRoutine;
+import com.nashss.se.fitnice.exceptions.InvalidAttributeException;
+import com.nashss.se.fitnice.exceptions.InvalidAttributeValueException;
+import com.nashss.se.fitnice.exceptions.WorkoutRoutineNotFoundException;
+import com.nashss.se.fitnice.metrics.MetricsConstants;
+import com.nashss.se.fitnice.metrics.MetricsPublisher;
+import com.nashss.se.fitnice.models.WorkoutRoutineModel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+
+public class UpdateWorkoutRoutineActivity {
+    private final Logger log = LogManager.getLogger();
+    private final WorkoutRoutineDao workoutRoutineDao;
+    private final MetricsPublisher metricsPublisher;
+
+    /**
+     * Instantiates a new UpdatePlaylistActivity object.
+     *
+     * @param playlistDao      PlaylistDao to access the playlist table.
+     * @param metricsPublisher MetricsPublisher to track exception occurrences.
+     */
+    @Inject
+    public UpdateWorkoutRoutineActivity(WorkoutRoutineDao workoutRoutineDao, MetricsPublisher metricsPublisher) {
+        this.workoutRoutineDao = workoutRoutineDao;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    /**
+     * This method handles the incoming request by retrieving the playlist, updating it,
+     * and persisting the playlist.
+     * <p>
+     * It then returns the updated playlist.
+     * <p>
+     * If the playlist does not exist, this should throw a PlaylistNotFoundException.
+     * <p>
+     * If the provided playlist name or customer ID has invalid characters, throws an
+     * InvalidAttributeValueException
+     * <p>
+     * If the request tries to update the customer ID,
+     * this should throw an InvalidAttributeChangeException
+     *
+     * @param updatePlaylistRequest request object containing the playlist ID, playlist name, and customer ID
+     *                              associated with it
+     * @return updatePlaylistResult result object containing the API defined {@link PlaylistModel}
+     */
+    public UpdateWorkoutRoutineResult handleRequest(final UpdateWorkoutRoutineRequest updateWorkoutRoutineRequest) {
+        log.info("Received UpdatePlaylistRequest {}", updateWorkoutRoutineRequest);
+
+        WorkoutRoutine workoutRoutine = workoutRoutineDao.getWorkoutRoutine(updateWorkoutRoutineRequest.getRoutineName());
+        if (workoutRoutine == null) {
+            throw new WorkoutRoutineNotFoundException("Playlist with " + updateWorkoutRoutineRequest.getRoutineName() + " not found.");
+        }
+        if (!MusicPlaylistServiceUtils.isValidString(updateWorkoutRoutineRequest.getRoutineName())) {
+            metricsPublisher.addCount(MetricsConstants.UPDATEWORKOUTROUTINE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+            throw new InvalidAttributeValueException("Illegal character found in name.");
+        }
+        metricsPublisher.addCount(MetricsConstants.UPDATEWORKOUTROUTINE_INVALIDATTRIBUTEVALUE_COUNT, 0);
+        if (!updateWorkoutRoutineRequest.getRoutineName().equals(workoutRoutine.getRoutineName())) {
+            metricsPublisher.addCount(MetricsConstants.UPDATEWORKOUTROUTINE_INVALIDATTRIBUTEVALUE_COUNT, 1);
+            throw new InvalidAttributeException("RoutineName does not match playlist RoutineName!");
+        }
+        metricsPublisher.addCount(MetricsConstants.UPDATEWORKOUTROUTINE_INVALIDATTRIBUTEVALUE_COUNT, 0);
+        workoutRoutine.setRoutineName(updateWorkoutRoutineRequest.getRoutineName());
+        workoutRoutine.setTags(updateWorkoutRoutineRequest.getTags());
+        workoutRoutine.setDescription(updateWorkoutRoutineRequest.getDescription());
+        workoutRoutine.setExercises(updateWorkoutRoutineRequest.getExercises());
+        WorkoutRoutine updatedNameWorkoutRoutine = workoutRoutineDao.saveWorkoutRoutine(workoutRoutine);
+        WorkoutRoutineModel workoutRoutineModel = new VModelConverter().toWorkoutRoutineModel(updatedNameWorkoutRoutine);
+
+        return UpdateWorkoutRoutineResult.builder()
+                .withWorkoutRoutine(workoutRoutineModel)
+                .build();
+    }
+}
---
+++ app/src/main/java/com/nashss/se/fitnice/activity/requests/UpdateWorkoutRoutineRequest.java
@@ -0,0 +1,84 @@
+package com.nashss.se.fitnice.activity.requests;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import java.util.List;
+
+@JsonDeserialize(builder = UpdateWorkoutRoutineRequest.Builder.class)
+public class UpdateWorkoutRoutineRequest {
+    private String routineName;
+    private List<String> tags;
+    private String description;
+    private List<String> exercises;
+
+    private UpdateWorkoutRoutineRequest(String routineName, List<String> tags, String description, List<String> exercises) {
+        this.routineName = routineName;
+        this.tags = tags;
+        this.description = description;
+        this.exercises = exercises;
+    }
+
+    public String getRoutineName() {
+        return routineName;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getExercises() {
+        return exercises;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdateWorkoutRoutineRequest{" +
+                "routineName='" + routineName + '\'' +
+                ", tags='" + tags + '\'' +
+                ", description='" + description + '\'' +
+                ", exercises='" + exercises + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder {
+        private String routineName;
+        private List<String> tags;
+        private String description;
+        private List<String> exercises;
+
+        public Builder withRoutineName(String routineName) {
+            this.routineName = routineName;
+            return this;
+        }
+
+        public Builder withTags(List<String> tags) {
+            this.tags = tags;
+            return this;
+        }
+
+        public Builder withDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder withExercises(List<String> exercises) {
+            this.exercises = exercises;
+            return this;
+        }
+
+        public UpdateWorkoutRoutineRequest build() {
+            return new UpdateWorkoutRoutineRequest(routineName, tags, description, exercises);
+        }
+    }
+}
